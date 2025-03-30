91199a4785717f175bdf672090dc5a107687a082
ravishrisanjay <ravishrisanjay@gmail.com>
2025-03-30 17:55:50 +0530

    Updated code with latest changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,142 +1,114 @@
-import React from 'react'
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import MenuIcon from '@mui/icons-material/Menu';
-import {useState} from 'react';
-
 import CloseIcon from '@mui/icons-material/Close';
+
 function Header() {
-    const[menu,setmenu]=useState(false);
-
-  return (
+    const [menu, setMenu] = useState(false);
+    
+    return (
         <>
-      <Container>
-        <a href="#">
-            <img src='/images/logo.svg' />
-        </a>
+            <Container>
+                <a href="#">
+                    <img src={`${process.env.PUBLIC_URL}/images/logo.svg`} alt="Tesla Logo" />
+                </a>
 
-        <Menu>
-            <a>MODEL 3</a>
-            <a>MODEL S</a>
-            <a>MODEL Y</a>
-            <a>MODEL X</a>
-        </Menu>
-    <Rightmenu>
-        <a>SHOP</a>
-        <a>TESLA ACCOUNT</a>
-        <Custommenu   onClick={()=>setmenu(true)}/>
-    </Rightmenu>
-    </Container>
-    <Listmenu  state={menu}>
-        <CloseWrap>
-
-        <Customclose   onClick={()=>setmenu(false)} />
-        </CloseWrap>
-
-        
-        
-        <li><a href='#'>Demo Drive</a></li>
-        <li><a href='#'>Demo Drive</a></li>
-        <li><a href='#'>Demo Drive</a></li>
-        <li><a href='#'>Demo Drive</a></li>
-        <li><a href='#'>Demo Drive</a></li>
-        <li><a href='#'>Demo Drive</a></li>
-        <li><a href='#'>Demo Drive</a></li>
-        <li><a href='#'>Demo Drive</a></li>
-    </Listmenu>
+                <Menu>
+                    <a>MODEL 3</a>
+                    <a>MODEL S</a>
+                    <a>MODEL Y</a>
+                    <a>MODEL X</a>
+                </Menu>
+                <RightMenu>
+                    <a>SHOP</a>
+                    <a>TESLA ACCOUNT</a>
+                    <CustomMenu onClick={() => setMenu(true)} />
+                </RightMenu>
+            </Container>
+            <ListMenu state={menu}>
+                <CloseWrap>
+                    <CustomClose onClick={() => setMenu(false)} />
+                </CloseWrap>
+                <li><a >Demo Drive</a></li>
+                <li><a >Insurance</a></li>
+                <li><a >Military Purchase Program</a></li>
+                <li><a >Video Guides</a></li>
+            </ListMenu>
         </>
-  )
+    );
 }
 
-export default Header
+export default Header;
 
-const Container=styled.div`
-z-index:1;
-min-height:60px;
-display:flex;
-align-items:center;
-background-img:none;
-justify-content:space-between;
-padding : 0 20px;
-position:fixed;
-top:0;
-left:0;
-right:0;
+const Container = styled.div`
+    z-index: 1;
+    min-height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
 `;
 
-const Menu=styled.div`
-display:flex;
-align-items:center;
-flex:1;
-justify-content:center;
-margin-left:140px;
-
-a{
-font-weight:800;
-cursor:pointer;
-text-transform:uppercase;
-padding:0 10px;
-flex-wrap:nowrap;
-font-size:25px;
-
-}
-@media(max-width:768px){
-display:none;
-}
-
+const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    flex: 1;
+    justify-content: center;
+    a {
+        font-weight: 800;
+        cursor: pointer;
+        text-transform: uppercase;
+        padding: 0 10px;
+        font-size: 18px;
+    }
+    @media (max-width: 768px) {
+        display: none;
+    }
 `;
 
-const Rightmenu=styled.div`
-
-display:flex;
-justify-content:center;
-align-items:center;
-a{
-font-weight:600;
-font-size:20px;
-
-text-transform:uppercase;
-padding:0 10px;
-flex-wrap:nowrap;
-cursor:pointer;
-
-
-}
-
+const RightMenu = styled.div`
+    display: flex;
+    align-items: center;
+    a {
+        font-weight: 600;
+        font-size: 16px;
+        text-transform: uppercase;
+        padding: 0 10px;
+        cursor: pointer;
+    }
 `;
 
-const Custommenu=styled(MenuIcon)`
-cursor:pointer;
-
-`;
-const Listmenu=styled.div`
-
-
-display:${props=>props.state?'flex':'none'};
-top:0;
-left:0;
-rigth:0;
-background-color:white;
-height:270px;
-width:100%;
-align-items:center;
-li{
-margin-left:50px;
-list-style:none;
-}
-a{
-font-weight:600;
-text-decration:none;
-}
-`;
-const Customclose=styled(CloseIcon)`
-cursor:pointer;
+const CustomMenu = styled(MenuIcon)`
+    cursor: pointer;
 `;
 
-const CloseWrap=styled.div`
-position:absolute;
-top: 50px; 
-  right: 0; 
-  margin-right:19px;
- 
+const ListMenu = styled.div`
+    display: ${props => props.state ? 'flex' : 'none'};
+    flex-direction: row;
+    background-color: white;
+    height: 270px;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    list-style: none;
+    align-items: center;
+    justify-content: center;
+    a{
+    padding :20px;
+    }
+`;
 
+const CustomClose = styled(CloseIcon)`
+    cursor: pointer;
+`;
+
+const CloseWrap = styled.div`
+    position: absolute;
+    top: 50px;
+    right: 20px;
 `;
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,108 +1,84 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 import { motion } from "framer-motion";
 
 function Section(props) {
-  return (
-    <Wrap bgimg={props.bgimg}>
-  <motion.div
-  initial={{ opacity: 0, y: 50 }}
-  animate={{ opacity: 1, y: 0 }}
-  transition={{ duration: 1, ease: "easeOut" }}
->
-
-        <Text>
-    <h1>{props.title}</h1>
-    <p>{props.descp} </p>
-        </Text>
-</motion.div>
-    <Button>
-    <motion.div
-  initial={{ opacity: 0, y: 50 }}
-  animate={{ opacity: 1, y: 0 }}
-  transition={{ duration: 1, ease: "easeOut" }}
->
-     <Buttongroup>
-        <Leftbutton>
-            {props.lbuton}
-        </Leftbutton>
-        <Rightbutton>
-            {props.rbuton}
-
-        </Rightbutton>
-        </Buttongroup>   
-        </motion.div>
-
-        <Downarrow src='/images/down-arrow.svg' />
-    </Button>
-
-   
-
-    </Wrap>
-  )
+    return (
+        <Wrap bgImg={`${process.env.PUBLIC_URL}/images/${props.bgimg}`}> 
+            <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, ease: "easeOut" }}>
+                <Text>
+                    <h1>{props.title}</h1>
+                    <p>{props.descp}</p>
+                </Text>
+            </motion.div>
+            <Button>
+                <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, ease: "easeOut" }}>
+                    <ButtonGroup>
+                        <LeftButton>{props.lbuton}</LeftButton>
+                        <RightButton>{props.rbuton}</RightButton>
+                    </ButtonGroup>
+                <DownArrow src={`${process.env.PUBLIC_URL}/images/down-arrow.svg`} />
+                </motion.div>
+            </Button>
+        </Wrap>
+    );
 }
 
-export default Section
-const Wrap=styled.div`
+export default Section;
 
-background-image:${props=>`url("/images/${props.bgimg}")`};
-
-width:100vw;
-height:100vh;
-background-size:cover;
-background-position:center;
-background-repeat:no-repeat;
-display:flex;
-flex-direction:column;
-justify-content:space-between;
-align-items:center;
-
-`
-const Text=styled.div`
-padding-top:15vh;
-text-align:center;
-font-size:30px;
-
-
+const Wrap = styled.div`
+    background-image: ${props => `url("${props.bgImg}")`};
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
 `;
 
-const Buttongroup=styled.div`
-display:flex;
-padding-bottom:30px;
-
-
-
-`;
-const Leftbutton=styled.div`
-background-color:rgba(23,26,32,0.8);
-height:40px;
-width:250px;
-color:white;
-display:flex;
-justify-content:center;
-align-items:center;
-border-radius:100px;
-opacity:0.85;
-font-size:12px;
-cursor:pointer;
-text-transform:uppercase;
-margin-right:10px;
-
-`;
-const Rightbutton=styled(Leftbutton)`
-background-color:white;
-color:black;
+const Text = styled.div`
+    padding-top: 15vh;
+    text-align: center;
 `;
 
-const Downarrow=styled.img`
-height:50px;
+const ButtonGroup = styled.div`
+    display: flex;
+    padding-bottom: 30px;
+`;
+
+const LeftButton = styled.div`
+    background-color: rgba(23,26,32,0.8);
+    height: 40px;
+    width: 250px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    font-size: 12px;
+    cursor: pointer;
+    text-transform: uppercase;
+    margin-right: 10px;
+`;
+
+const RightButton = styled(LeftButton)`
+    background-color: white;
+    color: black;
+`;
+
+const DownArrow = styled.img`
+ height:50px;
 margin-bottom:10px;
 margin-left:46%;
 animation:animatedown infinite 1.5s;
-`;
-
-const Button=styled.div`
-align-items:center;
 
 `;
 
+const Button = styled.div`
+
+align-items: center;
+
+`;
